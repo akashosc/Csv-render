b54775e0eee1e0ae1065f61d952f865640f2abce
akashosc <84351117+akashosc@users.noreply.github.com>
2023-04-18 00:54:40 +0530

    all the file are change into path.join dirname

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const PORT=8000;
 const mongoose=require('mongoose');
 const ejs=require('ejs');
 const ejslayouts=require('express-ejs-layouts');
-
+const path=require('path');
 const mongUrl="mongodb+srv://akash7067tiwari:pyRVxIqr7Z9qyvr3@cluster0.0nsntel.mongodb.net/?retryWrites=true&w=majority";
 
 // mongoose connection
 mongoose.connect(mongUrl,{useNewUrlParser:true}).then(()=>console.log('MongoDb connected'))
 .catch(err=>console.log(err));
 
-app.use(express.static('assiet'));
-app.use(express.static('uplodes'));
+// app.use(express.static('assiet'));
+
+app.use(express.static(path.join(__dirname ,'uplodes')));
 // views engine ejs
 app.use(ejslayouts);
 app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname , 'assiet')));
 
 
 
